131b9b750f30c262d0784d0e5cb01bc9fb1f541b
lwk <zscgrhg@qq.com>
2019-06-09 23:04:19 +0800

    555

--- src/main/java/com/zte/crm/prm/yaml/BootstrapConfig.java
+++ src/main/java/com/zte/crm/prm/yaml/BootstrapConfig.java
@@ -1,32 +1,54 @@
 package com.zte.crm.prm.yaml;
 
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.io.ClassPathResource;
 
+import java.util.List;
 import java.util.Properties;
 import java.util.stream.Stream;
 
 @Configuration
-public class BootstrapConfig   {
+@EnableConfigurationProperties(BootstrapConfig.class)
+@ConfigurationProperties(prefix = "app")
+public class BootstrapConfig {
+
+    List<String> ymllocaltion;
+
+    public List<String> getYmllocaltion() {
+        return ymllocaltion;
+    }
+
+    public void setYmllocaltion(List<String> ymllocaltion) {
+        this.ymllocaltion = ymllocaltion;
+    }
 
     @Bean
-    public  PropertySourcesPlaceholderConfigurer properties(ConfigurableEnvironment environment) {
+    public PropertySourcesPlaceholderConfigurer properties(ConfigurableEnvironment environment) {
         PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
         propertySourcesPlaceholderConfigurer.setIgnoreResourceNotFound(true);
 
-        Properties[] properties = Stream.of(new String[]{""}, environment.getActiveProfiles())
-                .flatMap(Stream::of).map(p -> {
-                    YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+        for (String s : ymllocaltion) {
+            System.out.println(s);
+        }
 
-                    String ps=p.isEmpty()?"":"-"+p;
-                    yaml.setResources(new ClassPathResource(String.format("config/app%s.yml", ps)));
+        Properties[] properties = Stream.of(new String[]{""}, environment.getActiveProfiles())
+                .flatMap(Stream::of)
+                .map(p -> {
+                    String ps = p.isEmpty() ? "" : "-" + p;
+                    ClassPathResource classPathResource = new ClassPathResource(String.format("config/app%s.yml", ps));
+                    return classPathResource;
+                }).filter(ClassPathResource::exists)
+                .map(p -> {
+                    YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+                    yaml.setResources(p);
                     return yaml.getObject();
                 }).toArray(Properties[]::new);
-
 
 
         propertySourcesPlaceholderConfigurer.setPropertiesArray(properties);
